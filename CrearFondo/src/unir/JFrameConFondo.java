3d0a7e71b322b55f6b3ea36d901d3e2294a7d306
wlarodri <wlarodri@hotmail.es>
2014-05-06 22:14:28 -0400

    Ahora si xd

---
+++ CrearFondo/src/unir/JFrameConFondo.java
@@ -0,0 +1,72 @@
+package unir;
+
+import java.awt.BorderLayout;
+import java.awt.EventQueue;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JOptionPane;
+import javax.swing.border.EmptyBorder;
+@SuppressWarnings({ "serial" })
+public class JFrameConFondo extends JFrame {
+		
+public static void main(String[] args) {
+EventQueue.invokeLater(new Runnable() {
+public void run() {
+try {
+JFrameConFondo frame = new JFrameConFondo();
+frame.setVisible(true);
+} catch (Exception e) {
+e.printStackTrace();
+}
+}
+});
+}
+
+
+
+public JFrameConFondo() {
+setTitle("Mi Primer Juego"); 
+
+setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+setBounds(100, 100, 1000, 600);
+PanelImagen p = new PanelImagen();
+p.setBorder(new EmptyBorder(0, 0, 0, 0));
+p.setLayout(new BorderLayout(50, 50));
+
+p.setLayout(null);
+JButton boton = new JButton("TUTORIAL");
+boton.setBounds(300,230,120,32);
+p.add(boton);
+JButton boton2 = new JButton("JUGAR");
+boton2.setBounds(560,230,120,32);
+p.add(boton2);
+JButton boton3 = new JButton("SALIR");
+boton3.setBounds(430,270,120,32);
+p.add(boton3);
+
+//Codigo accion Botones
+boton.addActionListener(new ActionListener(){
+public void actionPerformed(ActionEvent ae){
+JOptionPane.showMessageDialog(null, "                  Tutorial");
+}
+});
+boton2.addActionListener(new ActionListener(){
+public void actionPerformed(ActionEvent ae){
+ JOptionPane.showMessageDialog(null, "                   Jugar");
+}
+});
+boton3.addActionListener(new ActionListener(){
+public void actionPerformed(ActionEvent ae){
+ JOptionPane.showMessageDialog(null, "                Saliendo",null,JOptionPane.WARNING_MESSAGE);
+ 
+ System.exit(0);
+}
+});
+
+setContentPane(p);
+p.setSize(500,300);
+}
+}
---
+++ CrearFondo/src/unir/PanelImagen.java
@@ -0,0 +1,26 @@
+package unir;
+
+
+import java.awt.Dimension;
+import java.awt.Graphics;
+
+import javax.swing.ImageIcon;
+public class PanelImagen extends javax.swing.JPanel {
+
+	
+	private static final long serialVersionUID = 1L;
+	public static Object MostrarMensaje;
+public PanelImagen(){
+this.setSize(400,280);
+}
+@Override
+public void paintComponent (Graphics g){
+Dimension tamanio = getSize();
+ImageIcon imagenFondo = new ImageIcon(getClass().getResource("Fondoo.jpeg"));
+g.drawImage(imagenFondo.getImage(),0,0,tamanio.width, tamanio.height, null);
+setOpaque(false);
+super.paintComponent(g);
+}
+
+
+}
